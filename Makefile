0dece6d78ecb24aa27545e36725e94321dcbc2cb
fmeade <fmeade@radford.edu>
2016-04-15 23:23:28 -0400

    putput format

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
-
 all: cpusim
 
-master: cpusim.c ibarland-utils.o command-line-options.o
+cpusim: cpusim.c ibarland-utils.o command-line-options.o
 	gcc -Wall cpusim.c -o cpusim ibarland-utils.o command-line-options.o -lm
 
-command-line-options.o: command-line-options.c command-line-options.h ibarland-utils.o
+command-line-options.o: command-line-options.c command-line-options.h
 	gcc -Wall -c command-line-options.c
 
 ibarland-utils.o: ibarland-utils.c ibarland-utils.h
--- cpusim.c
+++ cpusim.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
 
 #include "command-line-options.h"
 #include "ibarland-utils.h"
@@ -21,22 +24,78 @@
  /* The possible command-line options to the program. 
  */
 struct option_info options[] =
-  {  { "numJobs"             , 'n', "5", "the number of jobs to complete, before ending the simulation. " }
-    ,{ "foreground-quantum"  , 'f', "100", "the time (in ms) jobs in the foreground queue get, 
-    										before being moved to the intermediate queue. " } 
-    ,{ "intermediate-quantum", 'i', "200", "the time (in ms) jobs in the intermediate queue get, 
-    										before being moved to the background queue. " } 
-    ,{ "job-length"          , 'l', "300", "the average job-length in ms. " } 
-    ,{ "job-frequency"       , 'r', "1", "the average number of new jobs per ms. " } 
-    ,{ "seed"                , 's', "1", "The random-number seed for the simulation. " } 
-    ,{ "verbose"             , 'v', "0", "enable verbose output or not. " } 
+  {  { "numJobs"             , 'n', "4", "the number of jobs to complete, before ending the simulation. " }
+    ,{ "foreground-quantum"  , 'f', "5", "the time (in ms) jobs in the foreground queue get, " 
+    										"before being moved to the intermediate queue. " } 
+    ,{ "intermediate-quantum", 'i', "10", "the time (in ms) jobs in the intermediate queue get, " 
+    										"before being moved to the background queue. " } 
+    ,{ "job-length"          , 'l', "10", "the expected job-length in ms. " } 
+    ,{ "job-frequency"       , 'r', "1", "the expected number of new jobs per ms. " } 
+    ,{ "seed"                , 's', NULL, "The random-number seed for the simulation. " } 
+    ,{ "verbose"             , 'v', "true", "enable verbose output or not. " } 
   };
+
+#define NUM_OPTIONS SIZEOF_ARRAY(options)
 
 int main(int argc, char** argv) {
 
 	char** settings = allOptions( argc, argv, NUM_OPTIONS, options );
     // Now, the array `settings` contains all the options, in order:
     // either taken from the command-line, or from the default given in `options[]`.
+
+	const int NUMJOBS = atoi(settings[0]);
+	const int FOREGROUND_QUANTUM = atoi(settings[1]);
+	const int INTERMEDIATE_QUANTUM = atoi(settings[2]);
+	const int JOB_LENGTH = atoi(settings[3]);
+	const int JOB_FREQUENCY = atoi(settings[4]);
+    bool VERBOSE;
+
+    if (strcmp(settings[6], "true") == 0)
+        VERBOSE = true;
+    else
+        VERBOSE = false;
+
+	int SEED;
+
+    // generates a random number for the seed if not provided
+    srandom(time(0));
+    if (settings[5] == NULL)
+        SEED = random();
+        //Now, the seed is a random integer
+    else
+        SEED = atoi(settings[5]);
+
+    int avg_job_length = 0;
+    int avg_job_turnaround = 0;
+
+
+
+
+    printf("\n%s\n", "--Input Parameters--");
+    printf("%s%d\n", "Seed: ", SEED);
+    printf("%s%d\n", "# of Jobs: ", NUMJOBS);
+    printf("%s%d%s\n", "Foreground quantum: ", FOREGROUND_QUANTUM, " ms");
+    printf("%s%d%s\n", "Intermediate quantum: ", INTERMEDIATE_QUANTUM, " ms");
+    printf("%s%d%s\n", "Job Length: ", JOB_LENGTH, " ms");
+    printf("%s%d%s\n", "Job Frequency: ", JOB_FREQUENCY, " ms");
+
+    printf("\n%s\n", "--Job Details--");
+    printf("%s%d%s\n", "Average Jog Length: ", avg_job_length, " ms");
+    printf("%s%d\n", "Average Turnaround Time: ", avg_job_turnaround);
+
+    if(VERBOSE) {
+        for(int i=1; i <= NUMJOBS; i++) {
+    	    printf("\n%s%d%s\n", "--Job ", i, " Output--");
+    	    printf("%s%d%s\n", "Arrival Time: ", 0, " ms");
+    	    printf("%s%s\n", "Queue Change? ", "Hmm");
+    	    printf("%s%d%s\n", "Finish Time: ", 0, " ms");
+    	    printf("%s%d\n", "Turnaround Time: ", 0);
+        }
+	}
+
+    printf("\n%s\n\n", "--End--");
+
+
 
 
 	return 0;
